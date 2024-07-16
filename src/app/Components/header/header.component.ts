8613014224094dfa6f5102abebb686f911e0cb1f
Xindref <dakotahmccrary@gmail.com>
2024-07-16 11:00:55 -0500

    adjusted header styles, and added functional routes to Leaderboard and Home

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  navigate(url: string) {
+    this.router.navigateByUrl(url);
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
