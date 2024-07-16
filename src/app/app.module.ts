06613674fd35794a40c09901e90508561b4cd1cf
Max McClintock <maxmcclintock@Maxs-MBP.lan>
2024-07-16 09:56:15 -0500

    Created Settings component and game-state service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SettingsComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/services/game-state.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+interface GameConfig {
+  genre: string;
+  difficulty: string;
+  rounds: number;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class GameStateService {
+  private initialConfig: GameConfig = {
+    genre: "Pop",
+    difficulty: "Medium",
+    rounds: 5,
+  };
+
+  private configSubject = new BehaviorSubject<GameConfig>(this.initialConfig);
+  config$ = this.configSubject.asObservable();
+
+  constructor() {}
+
+  updateConfig(newConfig: GameConfig) {
+    this.configSubject.next(newConfig);
+    console.log(newConfig);
+  }
+}
