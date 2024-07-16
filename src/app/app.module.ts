90c854439454e503e5224bea3277c409e5d6a84d
Xindref <dakotahmccrary@gmail.com>
2024-07-16 10:35:03 -0500

    Added HTML skeletons for all endpoints and included routing for all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from './Components/header/header.component';
+import { SettingsComponent } from './Components/settings/settings.component';
+import { LeaderboardComponent } from './Components/leaderboard/leaderboard.component';
+import { GameComponent } from './Components/game/game.component';
+import { ResultsComponent } from './Components/results/results.component';
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "settings", component: SettingsComponent},
+    {path: "game", component: GameComponent},
+    {path: "leaderboard", component: LeaderboardComponent},
+    {path: "results", component: ResultsComponent}
+ ];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, SettingsComponent, LeaderboardComponent, GameComponent, ResultsComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
