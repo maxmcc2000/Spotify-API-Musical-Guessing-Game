de4c73b24c4cda6b6b41a05a24016a6a9c59602c
Xindref <dakotahmccrary@gmail.com>
2024-07-17 11:43:44 -0500

    basic workings of the game component in, hot mess

--- src/app/Components/game/game.component.ts
+++ src/app/Components/game/game.component.ts
@@ -12,8 +12,15 @@ interface GameConfig {
   rounds: number;
 }
 
-const clientId = '5ed7e77c36ab4ad3881c3861b63b91a9';
-const clientSecret = 'a5757766312243e5b5a714ba98333fa3';
+interface SongInfo {
+  artists: string;
+  explicit: boolean;
+  name: string;
+  preview_url: string;
+}
+
+const clientId = '94cebb94925e4acf87b17551e9280009';
+const clientSecret = '31a321fb362f463e8ed18d2b9099e2ac';
 const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
 
 @Component({
@@ -28,10 +35,18 @@ export class GameComponent implements OnInit {
   rounds: number = 0;
   currentRound: number = 0;
   genre: string = '';
+  numberOfOptions: number = 0;
+
   token: string = '';
-  songPreviews: string[] = [];
+
+  songPreviews: [] = [];
   howls: Howl[] = [];
+
   loading: boolean = true;
+
+  gameSongs: SongInfo[] = [];
+  choiceOptions: string[] = [];
+  randomChoices: string[] = [];
 
 
   constructor(private router: Router, private gameService: GameStateService) { }
@@ -40,6 +55,19 @@ export class GameComponent implements OnInit {
     this.configSubscription = this.gameService.config$.subscribe((config: GameConfig) => {
       this.rounds = config.rounds;
       this.genre = config.genre;
+      switch (config.difficulty) {
+        case 'easy':
+          this.numberOfOptions = 2;
+          break;
+        case 'normal':
+          this.numberOfOptions = 4;
+          break;
+        case 'hard':
+          this.numberOfOptions = 0;
+          break;
+        default:
+          break;
+      }
     });
     this.getAccessToken();
   }
@@ -59,23 +87,35 @@ export class GameComponent implements OnInit {
     this.pullRandomSong();
   };
 
-  async getAllSongs() {
-    // console.log('Loading...');
-    // for (let i = 0; this.songPreviews.length < this.rounds; i++) {
-    //   let song = await this.pullRandomSong();
-    //   if (song['preview_url'] !== null && song['explicit'] === false) {
-    //     this.songPreviews.push(song['preview_url']);
-    //   }
-    // }
-    // console.log(this.songPreviews);
-    // this.loading = false;
-    // this.initializeHowls();
+  populateChoices() {
+    this.choiceOptions = [];
+    for (let i = 0; i < this.numberOfOptions - 1; i++) {
+      let randomIndex;
+      do {
+        randomIndex = Math.floor(Math.random() * this.randomChoices.length);
+      } while (this.choiceOptions.includes(this.randomChoices[randomIndex]));
+  
+      this.choiceOptions[i] = this.randomChoices[randomIndex];
+    }
+    this.choiceOptions.push(this.gameSongs[this.currentRound].name);
+
+    this.shuffleChoices(this.choiceOptions);
+  
+    console.log(this.choiceOptions);
+  }
+
+  shuffleChoices(songs: string[]) {
+    for (let i = songs.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [songs[i], songs[j]] = [songs[j], songs[i]];
+    }
+    return songs;
   }
 
   async pullRandomSong() {
     const params = {
       seed_genres: this.genre,
-      limit: 40,
+      limit: 100,
       market: 'US'
     }
     const endpoint = 'recommendations';
@@ -83,29 +123,48 @@ export class GameComponent implements OnInit {
     const response = await fetchFromSpotify({token: this.token, endpoint, params});
 
     if (response && response.tracks && response.tracks.length > 0) {
-      const songList = response.tracks;
-      this.songPreviews = songList;
+      this.songPreviews = response.tracks.filter((song: SongInfo) => {
+        return song.preview_url !== null && song.explicit === false
+      });
     }
+    this.songPreviews.forEach((song) => this.randomChoices.push(song['name']));
+    for (let i = 0; i < this.rounds; i++) {
+      let randomIndex = Math.floor((Math.random() * this.songPreviews.length) + 1);
+      this.gameSongs[i] = this.songPreviews[randomIndex];
+    }
+    this.initializeHowls();
   }
 
   initializeHowls(): void {
-    this.songPreviews.forEach(url => {
+    this.gameSongs.forEach(song => {
       const howl = new Howl({
-        src: [url],
+        src: [song['preview_url']],
         html5: true
       });
       this.howls.push(howl);
     });
+    console.log('howls initialized');
+    this.populateChoices();
   }
 
-  playSong(index: number) {
-    if (index >= 0 && index < this.howls.length) {
-      this.howls[index].play();
+  playSong() {
+      this.howls[this.currentRound].play();
+      console.log('should be playing');
+  }
+
+  checkAnswer(guessedSong: string) {
+    this.howls[this.currentRound].fade(1, 0, 200);
+    if (this.gameSongs[this.currentRound].name === guessedSong) {
+      console.log("Correct!");
+    } else {
+      console.log("Not Quite.");
     }
-  }
-
-  checkAnswer() {
     this.currentRound = this.currentRound+=1;
+    if (this.currentRound >= this.rounds) {
+      this.completeGame();
+      return;
+    }
+    this.populateChoices();
   }
 
   completeGame() {
